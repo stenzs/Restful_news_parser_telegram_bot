3f7b2a517a73fdb446dccb423b76349b1c251bed
stepanarviladze <80571319+stepanarviladze@users.noreply.github.com>
2021-06-24 15:09:17 +0500

    code improvement

--- app.py
+++ app.py
@@ -6,51 +6,51 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['subscribe'])
 async def subscribe(message: types.Message):
     b = str(message.from_user.id)
-    a = str(requests.post('http://127.0.0.1:80/users/' + b).text)
+    a = str(requests.post(domen + 'users/' + b).text)
     await message.answer(a)
 @dp.message_handler(commands=['unsubscribe'])
 async def unsubscribe(message: types.Message):
     b = str(message.from_user.id)
-    a = str(requests.delete('http://127.0.0.1:80/users/' + b).text)
+    a = str(requests.delete(domen + 'users/' + b).text)
     await message.answer(a)
 @dp.message_handler(commands=['addk'])
 async def subscribek(message: types.Message):
     keyw = ((message.text)[6:]).strip()
     b = str(message.from_user.id)
-    a = str(requests.post('http://127.0.0.1:80/keywords/' + b + '/' + keyw).text)
+    a = str(requests.post(domen + 'keywords/' + b + '/' + keyw).text)
     await message.answer(a)
 @dp.message_handler(commands=['delk'])
 async def unsubscribek(message: types.Message):
     keyw = ((message.text)[6:]).strip()
     b = str(message.from_user.id)
-    a = str(requests.delete('http://127.0.0.1:80/keywords/' + b + '/' + keyw).text)
+    a = str(requests.delete(domen + 'keywords/' + b + '/' + keyw).text)
     await message.answer(a)
 @dp.message_handler(commands=['showk'])
 async def showk(message: types.Message):
     b = str(message.from_user.id)
-    a = str(requests.get('http://127.0.0.1:80/keywords/' + b).text)
+    a = str(requests.get(domen + 'keywords/' + b).text)
     await message.answer(a)
 @dp.message_handler(commands=['addc'])
 async def subscribec(message: types.Message):
     kateg = ((message.text)[6:]).strip()
     b = str(message.from_user.id)
-    a = str(requests.post('http://127.0.0.1:80/categories/' + b + '/' + kateg).text)
+    a = str(requests.post(domen + 'categories/' + b + '/' + kateg).text)
     await message.answer(a)
 @dp.message_handler(commands=['delc'])
 async def unsubscribec(message: types.Message):
     kateg = ((message.text)[6:]).strip()
     b = str(message.from_user.id)
-    a = str(requests.delete('http://127.0.0.1:80/categories/' + b + '/' + kateg).text)
+    a = str(requests.delete(domen + 'categories/' + b + '/' + kateg).text)
     await message.answer(a)
 @dp.message_handler(commands=['showc'])
 async def showc(message: types.Message):
     b = str(message.from_user.id)
-    a = str(requests.get('http://127.0.0.1:80/categories/' + b).text)
+    a = str(requests.get(domen + 'categories/' + b).text)
     await message.answer(a)
 @dp.message_handler(commands=['newsc'])
 async def newsc(message: types.Message):
     b = str(message.from_user.id)
-    a = str(requests.get('http://127.0.0.1:80/newsc/' + b).text)
+    a = str(requests.get(domen + 'newsc/' + b).text)
     f = eval(a)
     for i in range(len(f)):
         if (f[i])[:28] == 'Топ 10 новостей по категории':
@@ -60,7 +60,7 @@ async def newsc(message: types.Message):
 @dp.message_handler(commands=['newsk'])
 async def newsk(message: types.Message):
     b = str(message.from_user.id)
-    a = str(requests.get('http://127.0.0.1:80/newsk/' + b).text)
+    a = str(requests.get(domen + 'newsk/' + b).text)
     f = eval(a)
     for i in range(len(f)):
         if (f[i])[:34] == 'Топ 10 новостей по ключевому слову':
